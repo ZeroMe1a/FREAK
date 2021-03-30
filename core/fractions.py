6a1cb23779f376f075001750b964c4583991286f
MauroTheCreator <maurotorresdev@gmail.com>
2021-03-30 19:22:50 -0300

    implementando novo tipo: Fractions
    Fiz uma classe para representar um tipo novo de dados que vai facilitar
    a nossa vida, em vez de loops, algoritmos, zips e etc, podemos
    simplesmente usar esse tipo pra criar frações e usar operadores
    built-in pra frações, simples e fácil (além de rápido).

--- core/fractions.py
+++ core/fractions.py
@@ -1,34 +1,30 @@
-# TODO: simp, mult, somar, sub
-
-class Fractions:
+# TODO: Implementar .simple(), plus, minus, div e operadores de comparação
+class Fraction:
     """
-    Fractions calculations are made here.
+    Fraction data type.
+
+    num -- fraction numerator
+    den -- fraction denominator
     """
 
-    def __init__(self) -> None:
-        self.time = __import__("time")
-        self.math = __import__("math")
+    def __init__(self, num: int, den: int):
+        """__init__ bound method for fractions."""
+        self.num = num
+        self.den = den
+
+    
+    def __mul__(self, fraction):
+        """__mul__ bound method for fractions."""
+        return Fraction(self.num * fraction.num,
+                        self.den * fraction.den)
+
+    def __repr__(self):
+        """__repr__ bound method for fractions."""
+
+        return f"""Fraction(num={self.num}, den={self.den})"""
 
 
-    def simp(self, den: int, num: int) -> list:
-        """
-        Simplify fractions
-        """
-        perf =  self.time.perf_counter()
-
-        simplified = list()
-
-        while self.math.gcd(den, num) > 1:
-            gcd = self.math.gcd(den, num)
-            simplified = [den / gcd, num / gcd]
-            if den - gcd > 0 and num - gcd > 0:
-                den -= gcd
-                num -= gcd
-            else:
-                perf = self.time.perf_counter()
-                return [simplified, perf]
-
-        perf = self.time.perf_counter()
-
-        return [[den, num], perf]
+    def __eq__(self, fraction):
+        """__eq__ bound method for fractions."""
+        return True if self.num == fraction.num and self.den == fraction.den else False
 
