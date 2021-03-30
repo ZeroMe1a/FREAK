26c0a2c2d95e451d1d6d37fcdf90ef8be6eff72e
MauroTheCreator <maurotorresdev@gmail.com>
2021-03-30 14:15:13 -0300

    implementing Fractions class
    Implementei a classe Fractions pra gerenciar as frações no programa,
    também implementei o método .simp() pra simplificar frações (retorna uma
    lista na forma [den, num] com a fração simplificada).

--- core/fractions.py
+++ core/fractions.py
@@ -1,59 +1,29 @@
-from math import gcd, lcm
-from time import perf_counter_ns as counter
+# TODO: simp, mult, somar, sub
+
+class Fractions:
+    """
+    Fractions calculations are made here.
+    """
+
+    def __init__(self) -> None:
+        self.time = __import__("time")
+        self.math = __import__("math")
 
 
-def simp(f):
+    def simp(self, den: int, num: int) -> list:
+        """
+        Simplify fractions
+        """        
 
-	t = counter()
+        simplified = list()
 
-	g = gcd(f[0], f[1])
+        while self.math.gcd(den, num) > 1:
+            gcd = self.math.gcd(den, num)
+            simplified = [den / gcd, num / gcd]
+            if den - gcd > 0 and num - gcd > 0:
+                den -= gcd
+                num -= gcd
+            else:
+                break
+        return simplified
 
-	f[0] = f[0] // g
-	f[1] = f[1] // g
-
-	return [f[0], f[1], counter()-t]
-
-
-def add(f1, f2):
-
-	t = counter()
-
-	if f1[1] == f2[1]:
-		return [f1[0]+f2[0], f1[1], counter()-t]
-	
-	else:
-		mmc = lcm(f1[1], f2[1])
-
-		f1[0] = mmc//f1[1]*f1[0]
-		f2[0] = mmc//f2[1]*f2[0]
-		
-		return [f1[0]+f2[0], mmc, counter()-t]
-
-
-def sub(f1, f2):
-
-	t = counter()
-
-	if f1[1] == f2[1]:
-
-		if max(f1[0], f2[0]) - min(f1[0], f2[0]) <= 0:
-			return [1, f1[1], counter()-t]
-
-		else:
-			return [max(f1[0], f2[0])-min(f1[0], f2[0]), f1[1], counter()-t]
-
-	elif f1[1] != f2[1]:
-		mmc = lcm(f1[1], f2[1])
-
-		f1[0] = mmc//f1[1]*f1[0]
-		f2[0] = mmc//f2[1]*f2[0]
-
-		if max(f1[0], f2[0]) - min(f1[0], f2[0]) <= 0:
-			return [1, mmc, counter()-t]
-		else:
-			return [max(f1[0], f2[0]) - min(f1[0], f2[0]), mmc, counter()-t]
-
-
-def mul(f1, f2):
-	t = counter()
-	return [f1[0]*f2[0], f1[1]*f2[1], counter()-t]
