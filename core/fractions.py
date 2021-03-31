6ce9dab460de723d062f51889854de479a750a52
MauroTheCreator <maurotorresdev@gmail.com>
2021-03-31 05:59:07 -0300

    novo método e operador suportado para Fraction
    implementei novo método para frações, ele retorna a fração simplificada
    e não falha nunca (baseei o método no algoritmo de euclides). Agora,
    comparação entre frações sempre serão feitas usando a forma reduzida de
    cada fração comparada, evitando erros. (antes 2 / 3 == 4 / 6 retornava
    false, apesar das frações serem iguais - pois uma estava na forma
    simplificada e outra não, aí dava esse erro).

--- core/fractions.py
+++ core/fractions.py
@@ -1,4 +1,4 @@
-# TODO: Implementar .simple(), plus, minus, div e operadores de comparação
+# TODO: Implementar plus, minus, div e operadores de comparação
 class Fraction:
     """
     Fraction data type.
@@ -11,20 +11,28 @@ class Fraction:
         """__init__ bound method for fractions."""
         self.num = num
         self.den = den
+        self.__math = __import__("math")
 
-    
+
     def __mul__(self, fraction):
         """__mul__ bound method for fractions."""
         return Fraction(self.num * fraction.num,
                         self.den * fraction.den)
 
-    def __repr__(self):
-        """__repr__ bound method for fractions."""
-
-        return f"""Fraction(num={self.num}, den={self.den})"""
-
 
     def __eq__(self, fraction):
         """__eq__ bound method for fractions."""
-        return True if self.num == fraction.num and self.den == fraction.den else False
+        return True if (self.simplify().num == fraction.simplify().num
+                    and self.simplify().den == fraction.simplify().den) else False
+
+
+    def __repr__(self):
+            """__repr__ bound method for fractions."""
+            return f"""Fraction(num={self.num}, den={self.den})"""
+
+
+    def simplify(self):
+        """Simplify the fraction object."""
+        return Fraction(self.num / self.__math.gcd(self.num, self.den),
+                        self.den / self.__math.gcd(self.num, self.den))
 
